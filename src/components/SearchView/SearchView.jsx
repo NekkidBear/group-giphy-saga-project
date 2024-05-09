18a1edc26b68cc453166e7dbe3ae4966c61f2124
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-05-09 14:21:07 -0500

    creating input handler, add local state

--- src/components/SearchView/SearchView.jsx
+++ src/components/SearchView/SearchView.jsx
@@ -1,8 +1,28 @@
+import { useDispatch } from "react-redux"
+import { useState } from "react";
+
 export default function SearchView(){
+    const dispatch = useDispatch();
+    const [searchTerms, setSearchTerms] = useState('')
+    const searchGiphy = () =>{
+        dispatch({
+            type: "FETCH_GIFS",
+            payload: searchTerms
+        })
+    }
+    const inputHandler=()=>{
+        //todo
+    }
     return(
         <div>
-            <p> Search View Goes here </p>
+            <form onSubmit={searchGiphy} className="searchForm">
+                <input onChange={inputHandler}
+                    type="text"
+                    placeholder="Search for "
+                    />
+                    <button>Show me the GIFS!</button>
+            </form>
         </div>
     )
-    //todo
+    
 }
