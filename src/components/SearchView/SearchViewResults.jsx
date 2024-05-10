be9df7c0fe56cc72efe5ff87f2cfd006a6052485
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-05-10 11:58:31 -0500

    comment out console.logs for tracing.

--- src/components/SearchView/SearchViewResults.jsx
+++ src/components/SearchView/SearchViewResults.jsx
@@ -3,13 +3,13 @@ import { useSelector } from "react-redux";
 
 function SearchViewResults() {
   const GIPHYs = useSelector(store=>store.GIPHYs);
-  console.log("in SearchViewResults")
-  console.log('GIPHYs is', GIPHYs)
+  // console.log("in SearchViewResults")
+  // console.log('GIPHYs is', GIPHYs)
   return (
     <div>
       <h3>Search Results</h3>
       {GIPHYs.map((gif)=>{
-        console.log("GIF item is: ", gif)
+        {/* console.log("GIF item is: ", gif) */}
         return (
             <div key={gif.id}>
                 <SV_ResultsItem {...gif} />
